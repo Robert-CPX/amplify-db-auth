38465065a520845111afba3d7a26fce3fc7bc765
Robert-CPX <cpx@robbdev.xyz>
2024-07-19 18:29:09 +0800

    commit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,22 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: "MicrosoftEntraID",
+          clientId: secret("MICROSOFT_ENTRA_ID_CLIENT_ID"),
+          clientSecret: secret("MICROSOFT_ENTRA_ID_CLIENT_SECRET"),
+          issuerUrl: "<your-issuer-url>",
+        },
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://mywebsite.com"],
+      callbackUrls: [
+        "http://localhost:3000/profile",
+        "https://mywebsite.com/profile",
+      ],
+    },
   },
 });
